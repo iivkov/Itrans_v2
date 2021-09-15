34cd9ed823b6ce3e1a8034854492dcc2222c7c2d
Ivan <ivan.ivkovic1996@gmail.com>
2021-09-15 16:24:59 +0200

    Migracija za tablicu orders

---
+++ database/migrations/2021_09_15_141611_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->date('departure_date');
+            $table->string('departure_location');
+            $table->date('arrival_date');
+            $table->string('arrival_location');
+            $table->string('order_info');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
