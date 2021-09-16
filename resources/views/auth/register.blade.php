0b9dd0eff0309363609c9cf334c90ef390725aaa
Ivan <ivan.ivkovic1996@gmail.com>
2021-09-16 21:09:03 +0200

    Lokalizacija na hrvatski

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,93 +14,95 @@
 
             <!-- Name -->
             <div>
-                <x-label for="name" :value="__('Name')" />
+                <x-label for="name" :value="__('Ime')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" placeholder="Unesite ime" required autofocus />
             </div>
 
             <!-- Surname -->
             <div>
-                <x-label for="surname" :value="__('Surname')" />
+                <x-label for="surname" :value="__('Prezime')" />
 
-                <x-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" required autofocus />
+                <x-input id="surname" class="block mt-1 w-full" type="text" name="surname" :value="old('surname')" placeholder="Unesite prezime" required autofocus />
             </div>
 
             <!-- VAT / OIB -->
             <div>
-                <x-label for="vat" :value="__('VAT')" />
+                <x-label for="vat" :value="__('OIB')" />
 
-                <x-input id="vat" class="block mt-1 w-full" type="text" name="vat" :value="old('vat')" required autofocus />
+                <x-input id="vat" class="block mt-1 w-full" type="text" name="vat" :value="old('vat')" placeholder="Unesite OIB" required autofocus />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
+                <x-label for="email" :value="__('E-pošta')" />
 
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" placeholder="Unesite e-poštu" required />
             </div>
 
             <!-- Telephone -->
             <div class="mt-4">
-                <x-label for="telephone" :value="__('Telephone')" />
+                <x-label for="telephone" :value="__('Broj telefona')" />
 
-                <x-input id="telephone" class="block mt-1 w-full" type="tel" name="telephone" :value="old('telephone')" required autofocus />
+                <x-input id="telephone" class="block mt-1 w-full" type="tel" name="telephone" :value="old('telephone')" placeholder="Unesite broj telefona" required autofocus />
             </div>
 
             <!-- Address -->
             <div>
-                <x-label for="address" :value="__('Address')" />
+                <x-label for="address" :value="__('Adresa')" />
 
-                <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" required autofocus />
+                <x-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" placeholder="Unesite adresu" required autofocus />
             </div>
 
             <!-- Postal code -->
             <div>
-                <x-label for="postal_code" :value="__('Postal code')" />
+                <x-label for="postal_code" :value="__('Poštanski broj')" />
 
-                <x-input id="postal_code" class="block mt-1 w-full" type="text" name="postal_code" :value="old('postal_code')" required autofocus />
+                <x-input id="postal_code" class="block mt-1 w-full" type="text" name="postal_code" :value="old('postal_code')" placeholder="Unesite poštanski broj" required autofocus />
             </div>
 
             <!-- Town -->
             <div>
-                <x-label for="town" :value="__('Town')" />
+                <x-label for="town" :value="__('Mjesto')" />
 
-                <x-input id="town" class="block mt-1 w-full" type="text" name="town" :value="old('town')" required autofocus />
+                <x-input id="town" class="block mt-1 w-full" type="text" name="town" :value="old('town')" placeholder="Unesite mjesto" required autofocus />
             </div>
 
             <!-- Country -->
             <div>
-                <x-label for="country" :value="__('Country')" />
+                <x-label for="country" :value="__('Država')" />
 
-                <x-input id="country" class="block mt-1 w-full" type="text" name="country" :value="old('country')" required autofocus />
+                <x-input id="country" class="block mt-1 w-full" type="text" name="country" :value="old('country')" placeholder="Unesite državu" required autofocus />
             </div>
 
             <!-- Password -->
             <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
+                <x-label for="password" :value="__('Zaporka')" />
 
                 <x-input id="password" class="block mt-1 w-full"
                                 type="password"
                                 name="password"
+                                placeholder="Unesite zaporku"
                                 required autocomplete="new-password" />
             </div>
 
             <!-- Confirm Password -->
             <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
+                <x-label for="password_confirmation" :value="__('Potvrdite zaporku')" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
+                                placeholder="Ponovno unesite zaporku"
                                 name="password_confirmation" required />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
+                    {{ __('Već ste registrirani?') }}
                 </a>
 
                 <x-button class="ml-4">
-                    {{ __('Register') }}
+                    {{ __('Registriraj se') }}
                 </x-button>
             </div>
         </form>
--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -1,4 +1,5 @@
 @extends('vehicles.layout')
+@extends('layouts.app')
  
 @section('content')
     <div class="row">
